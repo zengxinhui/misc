a0c0a75e8da0f731cb827ac97a7cad58cd649fcd
zengxinhui <zengxh@gmail.com>
2019-10-13 14:30:04 -0400

    Update 4clojure.clj

--- 4clojure.clj
+++ 4clojure.clj
@@ -1,4 +1,4 @@
-(fn [xs]
+(fn _73 [xs]
   (->> (-> (into xs (apply map vector xs))
            (conj (mapv #(get-in xs %) [[0 0] [1 1] [2 2]])
                  (mapv #(get-in xs %) [[0 2] [1 1] [2 0]])))
