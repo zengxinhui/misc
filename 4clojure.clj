4551fe565b4049fa0056fc78ac914304595674d0
zengxinhui <zengxh@gmail.com>
2019-10-13 14:24:10 -0400

    Update 4clojure.clj

--- 4clojure.clj
+++ 4clojure.clj
@@ -1,8 +1,8 @@
-(fn _73 [xs]
-  (let [xs1 (-> (into xs (apply map vector xs))
-                (conj (mapv #(get-in xs %) [[0 0] [1 1] [2 2]]))
-                (conj (mapv #(get-in xs %) [[0 2] [1 1] [2 0]])))
-        xs1 (->> (map #(apply hash-set %) xs1)
-                 (filter #(= 1 (count %)))
-                 (apply hash-set))]
-    (ffirst (disj xs1 #{:e}))))
+(fn [xs]
+  (->> (-> (into xs (apply map vector xs))
+           (conj (mapv #(get-in xs %) [[0 0] [1 1] [2 2]])
+                 (mapv #(get-in xs %) [[0 2] [1 1] [2 0]])))
+       (map distinct)
+       (filter #(= 1 (count %)))
+       flatten
+       (some #{:x :o})))
